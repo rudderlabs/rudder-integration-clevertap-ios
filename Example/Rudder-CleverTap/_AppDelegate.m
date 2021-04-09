e42cf9c8d7ef734cdd05cdb1716a2f4814e888be
Desu Sai Venkat <saivenkatdesu@gmail.com>
2021-04-09 17:32:40 +0530

    Minor Formatting changes

--- Example/Rudder-CleverTap/_AppDelegate.m
+++ Example/Rudder-CleverTap/_AppDelegate.m
@@ -18,7 +18,7 @@
 
 
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions 
 {
     // Override point for customization after application launch.
     NSString *WRITE_KEY = @"1qWrbpXGiDuf2xjaq3ti9CLBC53";
@@ -37,7 +37,8 @@
     center.delegate = self;
     [center requestAuthorizationWithOptions:(UNAuthorizationOptionAlert | UNAuthorizationOptionSound | UNAuthorizationOptionBadge)
                           completionHandler:^(BOOL granted, NSError * _Nullable error) {
-        if (granted) {
+        if (granted) 
+        {
             dispatch_async(dispatch_get_main_queue(), ^(void) {
                 [[UIApplication sharedApplication] registerForRemoteNotifications];
             });
@@ -172,47 +173,51 @@
 }
 
 
-- (void)applicationWillResignActive:(UIApplication *)application
+- (void)applicationWillResignActive:(UIApplication *)application 
 {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
 }
 
-- (void)applicationDidEnterBackground:(UIApplication *)application
+- (void)applicationDidEnterBackground:(UIApplication *)application 
 {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
 }
 
-- (void)applicationWillEnterForeground:(UIApplication *)application
+- (void)applicationWillEnterForeground:(UIApplication *)application 
 {
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
 }
 
-- (void)applicationDidBecomeActive:(UIApplication *)application
+- (void)applicationDidBecomeActive:(UIApplication *)application 
 {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
 }
 
-- (void)applicationWillTerminate:(UIApplication *)application
+- (void)applicationWillTerminate:(UIApplication *)application 
 {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
 
-- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken 
+{
     [[RudderCleverTapIntegration alloc] registeredForRemoteNotificationsWithDeviceToken:deviceToken];
 }
 
-- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler {
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler 
+{
     [[RudderCleverTapIntegration alloc] receivedRemoteNotification:userInfo];
     completionHandler(UIBackgroundFetchResultNoData);
 }
 
-- (void)userNotificationCenter:(UNUserNotificationCenter *)center willPresentNotification:(UNNotification *)notification withCompletionHandler:(void (^)(UNNotificationPresentationOptions))completionHandler {
+- (void)userNotificationCenter:(UNUserNotificationCenter *)center willPresentNotification:(UNNotification *)notification withCompletionHandler:(void (^)(UNNotificationPresentationOptions))completionHandler 
+{
     completionHandler(UNAuthorizationOptionSound | UNAuthorizationOptionAlert | UNAuthorizationOptionBadge);
 }
 
-- (void)userNotificationCenter:(UNUserNotificationCenter *)center didReceiveNotificationResponse:(UNNotificationResponse *)response withCompletionHandler:(void (^)(void))completionHandler {
+- (void)userNotificationCenter:(UNUserNotificationCenter *)center didReceiveNotificationResponse:(UNNotificationResponse *)response withCompletionHandler:(void (^)(void))completionHandler 
+{
     [[RudderCleverTapIntegration alloc] receivedRemoteNotification:response.notification.request.content.userInfo];
 }
 
--- Rudder-CleverTap/Classes/RudderCleverTapIntegration.m
+++ Rudder-CleverTap/Classes/RudderCleverTapIntegration.m
@@ -12,9 +12,11 @@
 
 #pragma mark - Initialization
 
-- (instancetype) initWithConfig:(NSDictionary *)config withAnalytics:(RSClient *)client withRudderConfig:(RSConfig *)rudderConfig {
+- (instancetype) initWithConfig:(NSDictionary *)config withAnalytics:(RSClient *)client withRudderConfig:(RSConfig *)rudderConfig 
+{
     self = [super init];
-    if (self) {
+    if (self) 
+    {
         dispatch_async(dispatch_get_main_queue(), ^{
             self.accountId = [config objectForKey:@"accountId"];
             self.accountToken = [config objectForKey:@"accountToken"];
@@ -26,7 +28,8 @@
                 {
                     [CleverTap setCredentialsWithAccountID:self.accountId token:self.accountToken region:self.region];
                 }
-                else{
+                else
+                {
                     [CleverTap setCredentialsWithAccountID:self.accountId andToken:self.accountToken];
                 }
                 [[CleverTap sharedInstance] notifyApplicationLaunchedWithOptions:nil];
@@ -34,11 +37,13 @@
                 {
                     [CleverTap setDebugLevel:CleverTapLogDebug];
                 }
-                else{
+                else
+                {
                     [CleverTap setDebugLevel:CleverTapLogInfo];
                 }
                 [RSLogger logDebug:@"Initializing CleverTap SDK"];
-            }else{
+            }else
+            {
                 [RSLogger logWarn:@"Failed to Initialize CleverTap Factory"];
             }
         });
@@ -46,25 +51,33 @@
     return self;
 }
 
-- (void) dump:(RSMessage *)message {
-    @try {
-        if (message != nil) {
+- (void) dump:(RSMessage *)message 
+{
+    @try 
+    {
+        if (message != nil) 
+        {
             dispatch_async(dispatch_get_main_queue(),^{
                 [self processRudderEvent:message];
             });
         }
-    } @catch (NSException *ex) {
+    } 
+    @catch (NSException *ex) 
+    {
         [RSLogger logError:[[NSString alloc] initWithFormat:@"%@", ex]];
     }
 }
 
-- (void)reset {
+- (void)reset 
+{
     [RSLogger logDebug:@"Inside reset"];
 }
 
-- (void) processRudderEvent: (nonnull RSMessage *) message {
+- (void) processRudderEvent: (nonnull RSMessage *) message 
+{
     NSString *type = message.type;
-    if([type isEqualToString:@"identify"]){
+    if([type isEqualToString:@"identify"])
+    {
         NSMutableDictionary *traits = [message.context.traits mutableCopy];
         NSMutableDictionary *profile = [[NSMutableDictionary alloc] init];
         NSString *userId     = message.userId;
@@ -73,32 +86,40 @@
             profile[@"Identity"] = userId;
             [traits removeObjectForKey:@"userId"];
             
-            if (traits[@"email"]) {
+            if (traits[@"email"]) 
+            {
                 profile[@"Email"] = traits[@"email"];
                 [traits removeObjectForKey:@"email"];
             }
             
-            if (traits[@"name"]) {
+            if (traits[@"name"]) 
+            {
                 profile[@"Name"] = traits[@"name"];
                 [traits removeObjectForKey:@"name"];
             }
             
-            if (traits[@"phone"]) {
+            if (traits[@"phone"]) 
+            {
                 profile[@"Phone"] = [NSString stringWithFormat:@"%@", traits[@"phone"]];
                 [traits removeObjectForKey:@"phone"];
             }
             
-            if ([traits[@"gender"] isKindOfClass:[NSString class]]) {
+            if ([traits[@"gender"] isKindOfClass:[NSString class]]) 
+            {
                 NSString *gender = traits[@"gender"];
-                if ([gender.lowercaseString isEqualToString:@"male"] || [gender.lowercaseString isEqualToString:@"m"]) {
+                if ([gender.lowercaseString isEqualToString:@"male"] || [gender.lowercaseString isEqualToString:@"m"]) 
+                {
                     profile[@"Gender"] = @"M";
-                } else if ([gender.lowercaseString isEqualToString:@"female"] || [gender.lowercaseString isEqualToString:@"f"]) {
+                } 
+                else if ([gender.lowercaseString isEqualToString:@"female"] || [gender.lowercaseString isEqualToString:@"f"]) 
+                {
                     profile[@"Gender"] = @"F";
                 }
                 [traits removeObjectForKey:@"gender"];
             }
             
-            if ([traits[@"birthday"] isKindOfClass:[NSString class]]) {
+            if ([traits[@"birthday"] isKindOfClass:[NSString class]]) 
+            {
                 NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
                 NSLocale *enUSPOSIXLocale = [NSLocale localeWithLocaleIdentifier:@"en_US_POSIX"];
                 [dateFormatter setLocale:enUSPOSIXLocale];
@@ -107,15 +128,18 @@
                 [traits removeObjectForKey:@"birthday"];
             }
             
-            else if ([traits[@"birthday"] isKindOfClass:[NSDate class]]) {
+            else if ([traits[@"birthday"] isKindOfClass:[NSDate class]]) 
+            {
                 profile[@"DOB"] = traits[@"birthday"];
                 [traits removeObjectForKey:@"birthday"];
             }
             
             // Only Primitive types, Date Object, String arrays are accepted
-            for (NSString *key in traits.allKeys) {
+            for (NSString *key in traits.allKeys) 
+            {
                 id value = traits[key];
-                if (![value isKindOfClass:[NSDictionary class]]) {
+                if (![value isKindOfClass:[NSDictionary class]]) 
+                {
                     profile[key] = value;
                 }
             }
@@ -123,23 +147,28 @@
             [[CleverTap sharedInstance] onUserLogin:profile];
         }
     }
-    else if([type isEqualToString:@"track"]){
+    else if([type isEqualToString:@"track"])
+    {
         NSString *eventName = message.event;
         if([eventName isEqualToString:@"Order Completed"])
         {
             [self handleEcommerceEvent:message];
         }
-        else{
+        else
+        {
             NSDictionary *eventProperties = message.properties;
             if(eventProperties)
             {
                 [[CleverTap sharedInstance] recordEvent:eventName withProps:eventProperties];
             }
-            else{
+            else
+            {
                 [[CleverTap sharedInstance] recordEvent:eventName];
             }
         }
-    }else if ([type isEqualToString:@"screen"]){
+    }
+    else if ([type isEqualToString:@"screen"])
+    {
         NSString *screenName = message.event;
         NSDictionary *screenProperties = message.properties;
         if(screenProperties)
@@ -147,11 +176,14 @@
             [[CleverTap sharedInstance] recordEvent:[NSString stringWithFormat:@"Viewed %@ Screen",
                                                      screenName] withProps:screenProperties];
         }
-        else{
+        else
+        {
             [[CleverTap sharedInstance] recordEvent:[NSString stringWithFormat:@"Viewed %@ Screen",
                                                      screenName]];
         }
-    }else {
+    }
+    else 
+    {
         [RSLogger logDebug:@"CleverTap Integration: Message type not supported"];
     }
     
@@ -179,27 +211,40 @@
 
 #pragma mark - Utils
 
--(void) handleEcommerceEvent:(RSMessage *) message {
+-(void) handleEcommerceEvent:(RSMessage *) message 
+{
     NSMutableDictionary *chargeDetails = [NSMutableDictionary new];
     NSArray *items = [NSArray new];
     
     NSDictionary *eventProperties = message.properties;
-    for (NSString *key in eventProperties.allKeys) {
+    for (NSString *key in eventProperties.allKeys) 
+    {
         id value = eventProperties[key];
-        if ([key isEqualToString:@"products"]) {
-            if (value != nil && [value isKindOfClass:[NSArray class]]) {
+        if ([key isEqualToString:@"products"]) 
+        {
+            if (value != nil && [value isKindOfClass:[NSArray class]]) 
+            {
                 NSArray *_products = (NSArray*) value;
-                if ([_products count] > 0) {
+                if ([_products count] > 0) 
+                {
                     items = [self getProductList:_products];
                 }
             }
-        } else if ([value isKindOfClass:[NSDictionary class]] || [value isKindOfClass:[NSArray class]]) {
+        } 
+        else if ([value isKindOfClass:[NSDictionary class]] || [value isKindOfClass:[NSArray class]]) 
+        {
             continue;
-        } else if ([key isEqualToString:@"order_id"]) {
+        } 
+        else if ([key isEqualToString:@"order_id"]) 
+        {
             chargeDetails[@"Charged ID"] = value;
-        } else if ([key isEqualToString:@"revenue"]) {
+        } 
+        else if ([key isEqualToString:@"revenue"]) 
+        {
             chargeDetails[@"Amount"] = value;
-        } else {
+        } 
+        else 
+        {
             chargeDetails[key] = value;
         }
     }
@@ -207,7 +252,8 @@
     [[CleverTap sharedInstance] recordChargedEventWithDetails:chargeDetails andItems:items];
 }
 
--(NSArray*) getProductList:(NSArray*) products {
+-(NSArray*) getProductList:(NSArray*) products 
+{
     NSMutableArray *transformedProducts = [[NSMutableArray alloc] init];
     for(NSDictionary *product in products)
     {
