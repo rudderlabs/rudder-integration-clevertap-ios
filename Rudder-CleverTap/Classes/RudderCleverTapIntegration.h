8c4e7c7ac093152ec6e7f73a5b610f7bdea286fb
Satheesh Kannan <satheesh@rudderstack.com>
2025-04-09 20:31:28 +0530

    chore: fix clevertap import issue on spm support

--- Rudder-CleverTap/Classes/RudderCleverTapIntegration.h
+++ Rudder-CleverTap/Classes/RudderCleverTapIntegration.h
@@ -13,6 +13,12 @@
 #import "Rudder.h"
 #endif
 
+// Below import is for the Rudder React Native CleverTap device mode.
+#if defined(__has_include) && __has_include(<CleverTap-iOS-SDK/CleverTap.h>)
+#import <CleverTap-iOS-SDK/CleverTap.h>
+#else
+#import "CleverTapSDK/CleverTap.h"
+#endif
 
 NS_ASSUME_NONNULL_BEGIN
 
--- Rudder-CleverTap/Classes/RudderCleverTapIntegration.m
+++ Rudder-CleverTap/Classes/RudderCleverTapIntegration.m
@@ -7,12 +7,6 @@
 
 #import "RudderCleverTapIntegration.h"
 
-// Below import is for the Rudder React Native CleverTap device mode.
-#if defined(__has_include) && __has_include(<CleverTap-iOS-SDK/CleverTap.h>)
-#import <CleverTap-iOS-SDK/CleverTap.h>
-#else
-#import "CleverTapSDK/CleverTap.h"
-#endif
 @implementation RudderCleverTapIntegration
 
 #pragma mark - Initialization
